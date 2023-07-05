74ad35fa9d49266288f47e4ac4ac7a2b49a87872
hamalxd <sammanhamal06@gmail.com>
2023-07-05 09:13:02 +0545

    Put a responsive navigation bar with API call

--- src/App.js
+++ src/App.js
@@ -1,13 +1,11 @@
 import './App.css';
 import Navigation from './components/Navigation/navigation';
-import Products from './components/Products/products';
 import APIcall from "./DataCall/callAPI"
 
 function App() {
   return (
     <>
     <Navigation/>
-    {/* <Products/> */}
     <APIcall/>
     </>
   );
